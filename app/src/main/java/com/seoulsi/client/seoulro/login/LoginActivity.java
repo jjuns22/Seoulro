f20d095bbb92e894561d292160b57994961d45ae
jjuns22 <rabent0509@gmail.com>
2017-10-03 14:45:28 +0900

    회원가입창 완료

---
+++ app/src/main/java/com/seoulsi/client/seoulro/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.seoulsi.client.seoulro.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seoulsi.client.seoulro.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/seoulsi/client/seoulro/login/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.seoulsi.client.seoulro.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seoulsi.client.seoulro.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
