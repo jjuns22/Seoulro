d062de41eb56b1f76a9996db56bbbcc1ba244715
jjuns22 <rabent0509@gmail.com>
2017-10-05 23:44:33 +0900

    회원가입 UI 완성

--- app/src/main/java/com/seoulsi/client/seoulro/login/LoginActivity.java
+++ app/src/main/java/com/seoulsi/client/seoulro/login/LoginActivity.java
@@ -1,15 +1,29 @@
 package com.seoulsi.client.seoulro.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.seoulsi.client.seoulro.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnLoginSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLoginSignUp = (Button)findViewById(R.id.btn_login_sign_up);
+        btnLoginSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/seoulsi/client/seoulro/login/SignUpActivity.java
+++ app/src/main/java/com/seoulsi/client/seoulro/login/SignUpActivity.java
@@ -2,14 +2,22 @@ package com.seoulsi.client.seoulro.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.seoulsi.client.seoulro.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText editTextSignupNickname;
+    EditText editTextSignupEmail;
+    EditText editTextSignupPwd;
+    EditText editTextSignupConfirmPwd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
     }
 }
