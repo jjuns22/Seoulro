0b894725ad04c96e231f597a38c772ab4b05804a
jjuns22 <rabent0509@gmail.com>
2017-10-05 15:44:35 +0900

    스플레시 완성

---
+++ app/src/main/java/com/seoulsi/client/seoulro/login/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.seoulsi.client.seoulro.login;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+import com.seoulsi.client.seoulro.R;
+import com.seoulsi.client.seoulro.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class); //LoginActivity 로 다시 바꿔야함
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+}
