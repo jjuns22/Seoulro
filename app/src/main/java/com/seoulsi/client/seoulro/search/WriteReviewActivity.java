7ecd87e57d8f2017a3dab44d4beac706e09148a7
jjuns22 <rabent0509@gmail.com>
2017-10-18 19:06:26 +0900

    글쓰기에서 사진첨부 구현 완료/ 앨범에 사진이 없어서 확인못함

--- app/src/main/java/com/seoulsi/client/seoulro/search/WriteReviewActivity.java
+++ app/src/main/java/com/seoulsi/client/seoulro/search/WriteReviewActivity.java
@@ -1,15 +1,179 @@
 package com.seoulsi.client.seoulro.search;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.BufferedOutputStream;
 import com.seoulsi.client.seoulro.R;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class WriteReviewActivity extends AppCompatActivity {
+
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_IMAGE = 2;
+
+    private Uri mImageCaptureUri;
+    private String absoultePath;
+
+    @BindView(R.id.btn_write_review_image_upload)
+    Button btnWriteReviewImageUpload;
+    @Nullable
+    @BindView(R.id.imageview_write_review_image)
+    ImageView imageViewWriteReviewImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+
+        ButterKnife.bind(this);
+
+        btnWriteReviewImageUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        doTakePhotoAction();
+                    }
+                };
+                DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        doTakeAlbumAction();
+                    }
+                };
+                DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                };
+
+                new AlertDialog.Builder(WriteReviewActivity.this)
+                        .setTitle("업로드할 이미지 선택")
+                        .setPositiveButton("사진 촬영", cameraListener)
+                        .setNeutralButton("앨범 선택", albumListener)
+                        .setNegativeButton("취소", cancelListener)
+                        .show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != RESULT_OK)
+            return;
+
+        switch (requestCode)
+        {
+            case PICK_FROM_ALBUM:
+            {
+                mImageCaptureUri = data.getData();
+            }
+            case PICK_FROM_CAMERA:
+            {
+                //이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정
+                //이후에 이미지 크롭 어플리케이션을 호출
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri,"image/*");
+
+                //CROP할 이미지를 200*200 크기로 저장
+                intent.putExtra("outputX",200); //CROP한 이미지의 x축 크기
+                intent.putExtra("outputY",200); //CROP한 이미지의 y축 크기
+                intent.putExtra("aspectX",1);   //CROP 박스의 X축 비율
+                intent.putExtra("aspectY",1);   //CROP 박스의 Y축 비율
+                intent.putExtra("scale",true);
+                intent.putExtra("return-data",true);
+                startActivityForResult(intent,CROP_FROM_IMAGE); //CROP_FROM_CAMERA case문 이동
+                break;
+            }
+            case CROP_FROM_IMAGE:
+            {
+                final Bundle extras = data.getExtras();
+
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+
+                        "/SmartWheel/"+System.currentTimeMillis()+".jpg";
+
+                if(extras != null)
+                {
+                    Bitmap photo = extras.getParcelable("data");s
+                    imageViewWriteReviewImg.setImageBitmap(photo);
+
+                    storeCropImage(photo, filePath);
+                    absoultePath = filePath;
+                    break;
+                }
+
+                //임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if(f.exists())
+                {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+    private void storeCropImage(Bitmap bitmap, String filePath)
+    {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/SmartWheel";
+        File directorySmartWheel = new File(dirPath);
+
+        if(!directorySmartWheel.exists())
+            directorySmartWheel.mkdir();
+
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try{
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            //sendBroadcast를 통해 CROPT된 사진을 앨범에 보이도록 갱신한다
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,Uri.fromFile(copyFile)));
+
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void doTakePhotoAction()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //임시로 사용할 파일의 경로를 생성
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,mImageCaptureUri);
+        startActivityForResult(intent,PICK_FROM_CAMERA);
+    }
+
+    public void doTakeAlbumAction()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent,PICK_FROM_ALBUM);
     }
 }
