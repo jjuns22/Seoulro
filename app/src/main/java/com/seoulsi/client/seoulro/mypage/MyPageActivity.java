3abe3ed43e24f6554ce1f54171440bbf5d671e08
jjuns22 <rabent0509@gmail.com>
2017-10-27 23:46:51 +0900

    로그아웃 추가

--- app/src/main/java/com/seoulsi/client/seoulro/mypage/MyPageActivity.java
+++ app/src/main/java/com/seoulsi/client/seoulro/mypage/MyPageActivity.java
@@ -1,7 +1,16 @@
 package com.seoulsi.client.seoulro.mypage;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.IntentCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,18 +25,26 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.seoulsi.client.seoulro.R;
 import com.seoulsi.client.seoulro.application.ApplicationController;
+import com.seoulsi.client.seoulro.login.LoginActivity;
 import com.seoulsi.client.seoulro.login.LoginUserInfo;
 import com.seoulsi.client.seoulro.main.MainActivity;
 import com.seoulsi.client.seoulro.mypage.Fragment.MyReviewFragment;
 import com.seoulsi.client.seoulro.mypage.Fragment.MySeoulroFragment;
 import com.seoulsi.client.seoulro.network.NetworkService;
 import com.seoulsi.client.seoulro.search.SearchActivity;
+import com.seoulsi.client.seoulro.search.WriteReviewActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MyPageActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_logout)
+    Button btnLogout;
+
     ViewPager vp;
     Button btnMyreview, btnMyseoul;
     private NetworkService service;
@@ -39,6 +56,8 @@ public class MyPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+
+        ButterKnife.bind(this);
         vp = (ViewPager)findViewById(R.id.viewpager_mypage);
         btnMyreview = (Button)findViewById(R.id.btn_myreview_mypage);
         btnMyseoul = (Button)findViewById(R.id.btn_myseoul_mypage);
@@ -52,6 +71,42 @@ public class MyPageActivity extends AppCompatActivity {
 
         home.setOnClickListener(click);
         search.setOnClickListener(click);
+
+        //로그아웃 버튼
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+                // builder.setTitle("AlertDialog Title");
+                builder.setMessage("정말 로그아웃 하시겠습니까??");
+                builder.setPositiveButton("예",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
+                                    //안드로이드 버전이 진저브레드가 아니면,
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                } else {
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                }
+                                SharedPreferences sp = MyPageActivity.this.getSharedPreferences("user", MODE_PRIVATE);
+                                sp.edit().remove("token").commit();
+
+                                startActivity(intent);
+                            }
+                        });
+                builder.setNegativeButton("아니오",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Toast.makeText(getApplicationContext(),"아니오를 선택했습니다.",Toast.LENGTH_LONG).show();
+                                dialog.dismiss();
+                            }
+                        });
+                builder.show();
+
+            }
+        });
+
 
         token = LoginUserInfo.getInstance().getUserInfo().token;
         Call<MyInfoResult> MyInfo = service.getMyInformation(token);
